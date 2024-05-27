a0f5d01d4c5fa4f5d6f328344024f7e98ca60b85
GabrielMoreau39 <gabriel.moreau@edu.univ-fcomte.fr>
2024-05-27 14:15:50 +0200

    ajout html footer

---
+++ Memobook/src/components/icons/FooterPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <div>
+        <IconLogo />
+        <div>
+            <p>CONTACT</p>
+            <p>A PROPOS</p>
+            <p>MENTIONS LÉGALES</p>
+            <p>CGV</p>
+        </div>
+        <div>
+            <p>Nous suivre</p>
+            <div>
+                <IconInstagram />
+                <IconFacebook />
+                <IconTwitter />
+            </div>
+        </div>
+    </div>
+</template>
